dd949efa0fd6b38699781f19c81921621a4e86de
Gabriel Back <42013655+GabrielBack28@users.noreply.github.com>
2022-08-16 10:01:05 -0300

    (feat) user controller

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { SaveUserDto } from '@modules/user/user.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { SaveUserDto, UpdateUserDto } from '@modules/user/user.dto';
 import { UserService } from '@modules/user/user.service';
 import { User } from '@app/entity/user.entity';
 
@@ -7,8 +16,31 @@ import { User } from '@app/entity/user.entity';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Get()
+  async findAll(): Promise<User[]> {
+    return await this.userService.find();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', new ParseUUIDPipe()) id: string): Promise<User> {
+    return await this.userService.findOneOrFail(id);
+  }
+
   @Post()
   async save(@Body() body: SaveUserDto): Promise<User> {
-    return this.userService.save(body);
+    return await this.userService.save(body);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() body: UpdateUserDto,
+  ): Promise<User> {
+    return await this.userService.update(id, body);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', new ParseUUIDPipe()) id: string): Promise<void> {
+    return await this.userService.deleteById(id);
   }
 }
